1309566dfd7beb50607b59345e786f69cfc5d28d
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-02-04 15:00:13 +0200

    4. Переопределение [#437669], v1.0

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,13 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return name + " : " + body + System.lineSeparator() + body + " : " + body;
+        return ("{" + System.lineSeparator() + "\"" + name + "\"" + " : " + "\""
+                + body + "\"" + "," + System.lineSeparator() + "\"" + body + "\"" + " : " + "\""
+                + body + "\"" + System.lineSeparator() + "}");
+    }
+
+    public static void main(String[] args) {
+        JSONReport js = new JSONReport();
+        System.out.println(js.generate("name", "body"));
     }
 }
