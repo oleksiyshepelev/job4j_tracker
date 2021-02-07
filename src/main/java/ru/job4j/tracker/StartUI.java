a4b1c91f92c038944c41b63ff3a5d92b7172e40a
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-02-07 23:44:32 +0200

    5. Tracker - хранилище [#437685]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,10 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println(currentDateTimeFormat);
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,37 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemsWithoutNull[size] = items[index];
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        for (int index = 0; index < itemsWithoutNull.length; index++) {
+            System.out.println(itemsWithoutNull[index]);
+        }
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index].getName().equals(key)) {
+                rsl[index] = items[index];
+                size++;
+                break;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
     }
 
     public Item findById(int id) {
