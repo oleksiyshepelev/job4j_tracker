86d07c5eb323570b666eab0cd4d31d490f183250
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-02-09 01:10:17 +0200

    9. toString [#437684]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,8 +3,6 @@ package ru.job4j.tracker;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        Tracker tracker = new Tracker();
-        tracker.add(item);
-        System.out.println(tracker.findById(1));
+        System.out.println(item);
     }
 }
