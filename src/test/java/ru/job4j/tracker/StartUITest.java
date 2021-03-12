ceeb17f371a7ceb9f12fa5442e3c7faffb3797f6
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-03-12 20:56:06 +0200

    9. Написать тесты на StartUI. [#437618] v.1.1

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -28,7 +28,7 @@ public class StartUITest {
         Item item = tracker.add(new Item(1, "Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[]{"0", "1", "New item name", "1"}
+                new String[]{"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
                 new EditAction(),
@@ -43,7 +43,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item(1, "Deleted item"));
         Input in = new StubInput(
-                new String[]{"0", "1", "1"}
+                new String[]{"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
