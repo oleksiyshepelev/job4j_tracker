efad2daf6f354532fc1ab09053b4b0272bfcd148
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-05-10 12:14:19 +0300

    3. Банковские переводы [#437594] v1.1

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,9 +9,7 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<>());
-        }
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
@@ -26,7 +24,7 @@ public class BankService {
 
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
-            if (user.getPassport().contains(passport)) {
+            if (user.getPassport().equals(passport)) {
                 return user;
             }
         }
@@ -53,25 +51,10 @@ public class BankService {
         Account src = findByRequisite(srcPassport, srcRequisite);
         Account dest = findByRequisite(destPassport, destRequisite);
         if (src != null && dest != null && src.getBalance() >= amount) {
-
+            src.setBalance(src.getBalance() - amount);
+            dest.setBalance(dest.getBalance() + amount);
+            rsl = true;
         }
-        /*
-        Метод для перечисления денег с одного счёта на другой счёт.
-        Если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят),
-        то метод должен вернуть false.
-         */
         return rsl;
     }
-
-    /*
-    Посмотрите на методы Map.putIfAbsent и List.contains, как их можно применить в этом задании.
-    Метод putIfAbsent позволяет проверить, если ли такой ключ в карте или нет и если его нет,
-    то произвести вставку данных.
-    Этот метод позволяет уменьшить количество кода.
-    Метод List.indexOf позволяет найти индекс элемента по значению.
-    Проверка элементов в этом методе происходит по методу equals.
-    Обратите внимание, что в моделях User и Account используется
-    только одно поле passport и requisite для сравнения объектов.
-    Это позволяет использовать эти методы, без информации о всех полях.
-     */
 }
