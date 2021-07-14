293ce4c9f05188e98a3d3f5941315498e3666bbc
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-07-14 21:43:22 +0300

    2.2. Лямбда блок [#249208] v1.1

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -13,7 +13,7 @@ public class LambdaUsage {
 
         Comparator<String> comparator = (left, right) -> {
             System.out.println("compare - " + left.length() + " : " + right.length());
-            return right.length() - left.length();
+            return Integer.compare(right.length(), left.length());
         };
     }
 }
